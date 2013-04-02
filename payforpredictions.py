2c7094f31b7e11eb4e2a42f9b4cc562c2a91e57b
Satrajit Ghosh <satra@mit.edu>
2013-04-02 18:00:46 -0400

    updated solution

--- payforpredictions.py
+++ payforpredictions.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
 # # Solving the pay for prediction challenge
 # 
 # This document is a collaboratively developed IPython Notebook to solve the ["Pay for prediction" challenge][PFPC].
@@ -6,11 +11,16 @@
 # 
 # [PFPC]: http://www.climatecentre.org/site/paying-for-predictions
 
+# <markdowncell>
+
 # ## Outline
 # 
 # - Overview of the challenge
 # - The simulator
 # - The solutions
+# - Factoring the problem
+
+# <markdowncell>
 
 # ### Overview
 # 
@@ -22,7 +32,8 @@
 #  
 # The Red Cross Red Crescent Climate Centre is launching a competition to find the best strategy for how to manage disaster risk in the game Paying for Predictions. This game demonstrates the many responsibilities of a humanitarian worker, from long-term preparedness to short-term anticipation of a disaster. In this global competition, we would like you to submit a strategy which you think will make a player win this game more often than other strategies.
 # 
-# 
+
+# <markdowncell>
 
 # #### How does this work?
 # 
@@ -31,6 +42,8 @@
 # A computer programme will use your strategy as if you were a person playing this game against other people over and over again. After many games are played, the programme will show which strategy wins most often.
 # 
 # We will announce the winners in March 2013. 
+
+# <markdowncell>
 
 # #### Competition rules
 # 
@@ -46,6 +59,8 @@
 # team.
 # - There are 10 rounds in this game.
 
+# <markdowncell>
+
 # #### Game setup
 # 
 # Each player receives 10 beans (resources), and one 
@@ -57,13 +72,15 @@
 # 
 # - The individual WINNER is the person with the most beans.
 # - The team WINNER is the team with fewest total  humanitarian crises. If there is a tie the team with most total beans combined is the team winner.
-# 
+
+# <markdowncell>
 
 # ### Simulator
 # 
 # The simulator is setup with some default strategies. These are for illustrative purposes.
 
-# In[1]:
+# <codecell>
+
 import numpy as np
 
 n_teams = 10
@@ -80,7 +97,8 @@ def initialize():
     drr_teams = np.ones((n_teams))  # have disaster risk reduction
     return beans, forecast_teams, drr_teams
 
-# In[2]:
+# <codecell>
+
 def get_forecast_bids(beans):
     """ Defines how each person or team will bid for regional forecast
 
@@ -105,10 +123,12 @@ def get_drr_bids(beans):
     bids = np.ones(beans.shape)
     return bids
 
+# <markdowncell>
 
 # ##### Gameplay: Stage 1 perform bids
 
-# In[3]:
+# <codecell>
+
 beans, forecast_teams, drr_teams = initialize()
 
 # perform forecast bids
@@ -122,13 +142,8 @@ beans = beans - (forecast_teams[:, None] * forecast_bids)
 print beans.T
 print forecast_teams
 
-# Out[3]:
-#     [[ 10.  10.  10.  10.  10.   9.   9.   9.   9.   9.]
-#      [ 10.  10.  10.  10.  10.   9.   9.   9.   9.   9.]
-#      [ 10.  10.  10.  10.  10.   9.   9.   9.   9.   9.]]
-#     [ 0.  0.  0.  0.  0.  1.  1.  1.  1.  1.]
-# 
-# In[4]:
+# <codecell>
+
 # perform drr bids
 drr_bids = get_drr_bids(beans)
 drr_team_bids = np.sum(drr_bids, axis=1)
@@ -141,16 +156,12 @@ print beans.T
 print forecast_teams
 print drr_teams
 
-# Out[4]:
-#     [[ 10.  10.  10.  10.  10.   9.   9.   9.   8.   8.]
-#      [ 10.  10.  10.  10.  10.   9.   9.   9.   8.   8.]
-#      [ 10.  10.  10.  10.  10.   9.   9.   9.   8.   8.]]
-#     [ 0.  0.  0.  0.  0.  1.  1.  1.  1.  1.]
-#     [ 0.  0.  0.  0.  0.  0.  0.  0.  1.  1.]
-# 
+# <markdowncell>
+
 # #### Round
 
-# In[5]:
+# <codecell>
+
 def get_insurance_payments(regional_predictions, drr_teams, beans, round_idx):
     # determine the likelihood of a flood
     likelihood = (7 - (target_rain - regional_predictions)) / 6
@@ -158,7 +169,8 @@ def get_insurance_payments(regional_predictions, drr_teams, beans, round_idx):
     payments = ((likelihood > .2) + (regional_predictions < 1))[:, None] * np.ones(beans.shape)
     return (payments * (beans > 0)).astype(int)
 
-# In[6]:
+# <codecell>
+
 def generate_rainfall(n_sides):
     regional_rainfall = np.random.randint(1, n_sides, size=(n_teams))
     local_rainfall = np.random.randint(1, 7, size=(n_teams, n_persons_per_team))
@@ -181,9 +193,12 @@ def adjust_beans(beans, payments, flooded, round_idx, drr_teams):
     in_crisis = already_in_crisis - beans_joining_crisis
     return beans + in_crisis
 
+# <markdowncell>
+
 # #### Simulate
 
-# In[7]:
+# <codecell>
+
 print beans.T
 for turn in range(n_rounds):
     n_sides = 6
@@ -196,29 +211,8 @@ for turn in range(n_rounds):
         print turn + 1, flooded.T - payments.T
 print beans.T
 
-# Out[7]:
-#     [[ 10.  10.  10.  10.  10.   9.   9.   9.   8.   8.]
-#      [ 10.  10.  10.  10.  10.   9.   9.   9.   8.   8.]
-#      [ 10.  10.  10.  10.  10.   9.   9.   9.   8.   8.]]
-#     1 [[-1 -1 -1  0 -1  0  0  0  0  0]
-#      [-1 -1 -1  0 -1  0  0  0 -1  0]
-#      [-1 -1  0 -1 -1  0  0  0 -1  0]]
-#     3 [[-1 -1 -1  0 -1  0 -1  0  0  0]
-#      [-1 -1 -1  0 -1  0 -1  0  0  0]
-#      [-1  0 -1  0 -1  0 -1  0  0  0]]
-#     5 [[-1 -1 -1 -1  0  0  0  0  0 -1]
-#      [-1 -1 -1  0  0  0 -1  0  0 -1]
-#      [-1 -1 -1 -1 -1  0  0  0  1  0]]
-#     7 [[-1 -1 -1  0 -1  0  0  0 -1  0]
-#      [-1 -1 -1  0 -1  0  0  0  0  1]
-#      [-1 -1 -1  0  0  0  0  0 -1  0]]
-#     9 [[-1 -1  0 -1  0  0  0  0  0  0]
-#      [-1 -1 -1 -1 -1  0  0  0  0  0]
-#      [-1 -1  0 -1 -1  0  0  0  0  0]]
-#     [[ 0.  0.  0.  0.  0.  8.  2.  7.  3.  5.]
-#      [ 0.  0.  0.  0.  0.  4.  2.  7.  5.  3.]
-#      [ 0.  0.  0.  0.  0.  8.  2.  7.  3.  5.]]
-# 
+# <markdowncell>
+
 # ### Solutions
 # 
 # Example from challenge guidelines
@@ -236,7 +230,8 @@ print beans.T
 #     If I don't have DRR and the dice rolls more than 5 and the rounds played are more than 6, then take 
 #     early action.
 #     Else, take no early action.
-# 
+
+# <markdowncell>
 
 # #### Solution from the strategies chosen for the simulation
 # 
@@ -249,11 +244,14 @@ print beans.T
 #     If I have forecast and the rolled dice is more than 4 then take early action.
 #     If I don't have forecast then take early action.
 #     By default take no action.
-# 
+
+# <markdowncell>
 
 # ## Factoring the problem
 # 
 # The problem can be factored into five independent strategies: one for each of the four possible bidding outcomes and one for placing the initial bid.
+
+# <markdowncell>
 
 # ### Strategy 1: winning no bids
 # 
@@ -265,19 +263,23 @@ print beans.T
 # 
 # The expected final bean count (neglecting running out of beans) is $10-6\left(\frac{7}{9}\right)-4 = \frac{4}{3}$.
 
-# ### Strategy 2: DDR only
+# <markdowncell>
+
+# ### Strategy 2: DRR only
 # 
-# For rounds 1-2, DDR is not available, so strategy 1 applies.
+# For rounds 1-2, DRR is not available, so strategy 1 applies.
 # 
 # For rounds 3-6, the expected cost of no insurance is $2\left(\frac{7}{36}\right) = \frac{7}{18} < 1$, so we should never buy insurance.
 # 
-# For rounds 7-10, the expected cost of no insurance is $2\left(\frac{15}{48}\right) = \frac{5}{8} > 1$, so we should never buy insurance.
+# For rounds 7-10, the expected cost of no insurance is $2\left(\frac{15}{48}\right) = \frac{5}{8} < 1$, so we should never buy insurance.
 # 
 # *I.e.*, the net strategy is to never buy insurance.
 # 
 # The expected final bean count (neglecting running out of beans) is $10-2\left(\frac{7}{9}\right)-4\left(\frac{7}{18}\right)-4\left(\frac{5}{8}\right) \approx 4.39$.
 # 
 # This suggests that DDR is worth slightly more than three beans.
+
+# <markdowncell>
 
 # ### Strategy 3: Forecast only
 # 
@@ -289,11 +291,13 @@ print beans.T
 # 
 # This suggests that the forecast is worth slightly less than 4 beans.
 
-# ### Strategy 4: DDR and Forecast
+# <markdowncell>
+
+# ### Strategy 4: DRR and Forecast
 # 
 # Again, rounds 1-2 match strategy 3.
 # 
-# For the DDR rounds:
+# For the DRR rounds:
 # 
 # * Given a regional forecast $\leq 5$, the expected cost of no insurance is $\leq \frac{2}{3}$, so we should never buy insurance.
 # * Given a regional forecast $\geq 7$, the expected cost of no insurance is $\geq \frac{4}{3}$, so we should always buy insurance.
@@ -302,11 +306,13 @@ print beans.T
 # 
 # The expected final bean count is: $10 - 2\left(\frac{1}{6}\frac{2}{3}+\frac{1}{3}\right)-4\left(\frac{1}{6}\frac{1}{3}+\frac{1}{6}\frac{2}{3}+\frac{1}{6}\right)-4\left(\frac{1}{8}\frac{1}{3}+\frac{1}{8}\frac{2}{3}+\frac{3}{8}\right) \approx 5.77$
 # 
-# This suggests that, given the forecast, DDR is worth less than one bean.
+# This suggests that, given the forecast, DRR is worth less than one bean.
+
+# <markdowncell>
 
 # ### Strategy 5: Initial bid
 # 
-# Because there is no tracking of opponents across games, there is no reason to change the opening bid between games (as there would be in, *e.g.*, the tit-for-tat strategy in the Prisoner's Dilema).  Therefore, the initial bid strategy is a choice among the $11^2 = 121$ possible opening bids.  Although this is the only
+# Because there is no tracking of opponents across games, there is no reason to change the opening bid between games (as there would be in, *e.g.*, the tit-for-tat strategy in the Prisoner's Dilemma).  Therefore, the initial bid strategy is a choice among the $11^2 = 121$ possible opening bids.  Although this is the only
 # stage of the game with any interaction with the other players, I think that it is still not an interactive choice.  That is, independently for each of the two
 # bids, it should always be optimal to bid the "fair market value" of the resource (*i.e.*, the difference in expected final beans with or without the resource).
 # Winning the resource by overbidding will tend to put our final score below an optimal play without the resource.  Underbidding *may* be a reasonable strategy as
@@ -314,5 +320,22 @@ print beans.T
 # 
 # From the expectation values above, neither resource should ever be worth more than three beans; so, the interesting space to explore has $4^2 = 16$ choices.
 
-# In[8]:
+# <markdowncell>
+
+# #### Solution
+# 
+#     # This is a complete example submission for the "Paying for Predictions" game.
+#     # Blank lines or lines beginning with "#" are ignored.
+#     # Bids:
+#     Bid 2 for the forecast.
+#     Bid 2 beans for DRR.
+#     # Conditions:
+#     If I have neither DRR nor forecast and round greater than 6 then take early action.
+#     If I have forecast and the rolled dice is more than 4 then take early action.
+#     If I have DRR and forecast and round greater than 6 and dice greater than 6 then take early action.
+#     By default take no action.
+
+# <codecell>
+
 !/software/challenges/nbconvert/nbconvert.py -f reveal /software/challenges/payforpredictions/payforpredictions.ipynb
+
